b4b5612c8a39bf7df36c9bf877fed3e5a9af8c24
luca <piergianluca@gmail.com>
2018-07-23 13:20:11 +0200

    update research notes, bib, scripts, plots

--- study_isingprior1.R
+++ study_isingprior1.R
@@ -32,6 +32,51 @@ dir.create(workdir)
 dataf <- c('data/BEN_T7C1.mat', 'data/BEN_T8C1.mat')
 binwidth <- 0.003
 
+calcevidence <- function(dataf,par1,par2){
+    ## read the spike times and find corresponding bins
+    nn <- 2
+    spikes <- sapply(1:nn,function(i){readMat(dataf[i])$cellTS %/% binwidth})
+
+    ## set first bin at 1 and find number of bins
+    tempmin <- min(c(unlist(spikes)))
+    n <- max(c(unlist(spikes)))-tempmin+1
+
+    ## sparse matrix with activity sequences (logical); one column per neuron
+    train <- sparseMatrix(i=(unlist(spikes)-tempmin+1), j=unlist(sapply(1:nn,function(i){rep(i,length(spikes[[i]]))})))
+
+    ## sparse vector with sequence of states
+    states <- as(c(Inf,train[,1]+2*train[,2]),"sparseVector")
+
+    ## frequency of state s after obs observations
+    freq <- function(s,obs){substates <- states[1:(obs+1)]
+        length(substates[c(substates)==s])}
+
+    ## log-evidence after obs observations
+    logevidence <- function(obs,l,f){lgamma(l)-lgamma(l+obs)+
+                                  sum(sapply(0:3,function(i){lgamma(l*f+freq(i,obs))-lgamma(l*f)}))}
+
+    lev1 <- logevidence(n,par1[1],par1[2])
+    lev2 <- logevidence(n,par2[1],par2[2])
+    c(lev1, lev2, lev1-lev2)
+}
+
+collectevidences <- function(dir,par1,par2,nsamples,savedir='',label='',seed=999){
+    set.seed(seed)
+    filelist <- list.files(path=dir,pattern='T.*\.mat')
+    nfiles <- length(filelist)
+
+    mat <- matrix(NA,nsamples,2+3)
+    for(i in 1:nsamples){
+        filenumbers <- sample(1:nfiles,2)
+        dataf <- filelist[filenumbers]
+        mat[i,] <- c(filenumbers, calcevidence(dataf,par1,par2))
+    }
+    
+    write.table(mat,paste0(savedir,'evidences_',label,'.csv'),sep=',',row.names=F,col.names=F,na='Infinity')
+}
+
+stop('end of script')
+
 ## read the spike times and find corresponding bins
 nn <- length(dataf)
 spikes <- sapply(1:nn,function(i){readMat(dataf[i])$cellTS %/% binwidth})
@@ -49,14 +94,19 @@ states <- as(c(Inf,train[,1]+2*train[,2]),"sparseVector")
 freq <- function(s,obs){substates <- states[1:(obs+1)]
     length(substates[c(substates)==s])}
 
-freqs <- function(obs){sapply(0:3,function(i){freq(i,obs)})}
+# freqs <- function(obs){sapply(0:3,function(i){freq(i,obs)})}
 
-prob <- function(s,obs,l,f){(l*f+freq(s,obs))/(l+obs)}
+# prob <- function(s,obs,l,f){(l*f+freq(s,obs))/(l+obs)}
 
-a <- 0; la <- 1e-12; nu <- rep(1,4)/4
-for(i in 1:n){cat('\r',i)
-    a <- a -log(nu[states[i+1]+1])
-    nu <- (la*nu + (states[i+1]==(0:3)))/(la+1)
-    la <- la + 1}
-a/n
-saveRDS(a/n,"suprise_prior_mu.rds")
+## a <- 0; la <- 1e-12; nu <- rep(1,4)/4
+## for(i in 1:n){cat('\r',i)
+##     a <- a -log(nu[states[i+1]+1])
+##     nu <- (la*nu + (states[i+1]==(0:3)))/(la+1)
+##     la <- la + 1}
+## a/n
+## saveRDS(a/n,"suprise_prior_mu.rds")
+
+logevidence <- function(obs,l,f){lgamma(l)-lgamma(l+obs)+
+                                  sum(sapply(0:3,function(i){lgamma(l*f+freq(i,obs))-lgamma(l*f)}))}
+
+calcevidence()
