96ea18799612091e85359911fe49f405bb3a1fc6
luca <piergianluca@gmail.com>
2018-07-28 23:25:12 +0200

    update text and scripts

--- overlearning/overlearning4.R
+++ overlearning/overlearning4.R
@@ -134,8 +134,8 @@ averagefromdata <- function(pfreqs,priorf,nsamples=100,nshuffles=100,label='',pp
     lallres
 }
 
- pfreqs <- matrix(c(1,1,8,4,4,2),3,2)/10
-totals <- averagefromdata(pfreqs,prior,nsamples=100,nshuffles=50000,label='testpar')
+# pfreqs <- matrix(c(1,1,8,4,4,2),3,2)/10
+#totals <- averagefromdata(pfreqs,prior,nsamples=100,nshuffles=50000,label='testpar')
 
 ## nshuffles = 100 * 5e3
 ## 3 -> abs.tol=1e-52
