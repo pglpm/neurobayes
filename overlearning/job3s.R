fc7daaf4d878c07c15a252568b9068ea20042529
luca <piergianluca@gmail.com>
2018-07-29 09:25:09 +0200

    update text and scripts

--- overlearning/job3s.R
+++ overlearning/job3s.R
@@ -3,4 +3,4 @@ source('overlearning4s.R')
 pfreqs <- matrix(c(1,1,8,4,4,2),3,2)/10
 
 ## nshuffles = 100 * 5e3
-totals <- averagefromdata(pfreqs,prior,nsamples=100,nshuffles=500,label='testparallel')
+totals <- averagefromdata(pfreqs,prior,nsamples=100,nshuffles=500,label='testserial')
