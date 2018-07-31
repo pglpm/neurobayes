f583aeb996a8b7eafbed064020c0cda074f371ba
luca <piergianluca@gmail.com>
2018-07-31 14:20:01 +0200

    update text and scripts

---
+++ overlearning/jobsd100onew20.R
@@ -0,0 +1,6 @@
+source('overlearning4.R')
+
+pfreqs <- matrix(c(1/2,1/2,10,5,5,1),3,2)/11
+
+## nshuffles = 100 * 5e3
+totals <- averagenewdata(pfreqs,prior,nsamples=20,nshuffles=1e6,label='sd100onew20',cores=20)
