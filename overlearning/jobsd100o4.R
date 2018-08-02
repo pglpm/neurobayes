41839a3990a6fbe19c0be4d9a0f3a77d708860ef
luca-hippo <pglpm0@gmail.com>
2018-08-02 06:59:31 +0200

    update data

--- overlearning/jobsd100o4.R
+++ overlearning/jobsd100o4.R
@@ -1,11 +1,11 @@
 source('overlearning4.R')
 
 pfreqs <- matrix(c(
-    c(1/2,1/2)*1/4, 3/4,
-    c(1/2,1/2)*4/5, 1/5
+    c(1/2,1/2)*0, 1,
+    c(1/2,1/2)*1, 0
 ),3,2)
 
-prior3 <- function(t){dnorm(t,mean=0,sd=1)}
+prior3 <- function(t){dnorm(t,mean=0,sd=1.686834785358595)}
 
 ## nshuffles = 100 * 5e3
-totals <- averagenewdata(pfreqs=pfreqs,priorf=prior3,pr=pr2,nsamples=20,nshuffles=5000,label='sd1o5',cores=20)
+totals <- averagenewdata(pfreqs=pfreqs,priorf=prior3,pr=pr2,nsamples=100,nshuffles=1000,label='sd1centr0',cores=20)
