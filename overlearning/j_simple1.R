c1b6a6ae37f876b77ddd3d158df246bd23f083bc
luca-hippo <pglpm0@gmail.com>
2018-08-02 17:45:42 +0200

    update data

--- overlearning/j_simple1.R
+++ overlearning/j_simple1.R
@@ -1,8 +1,8 @@
 source('overlearning_simple1.R')
 
-pp1 <- c(c(1/2,1/2)*3/4,1/4)
+pp1 <- c(c(1/2,1/2)*4/4, 0/4)
 
-prior1 <- function(t){dnorm(t,mean=0,sd=100)}
+prior1 <- function(t){dnorm(t,mean=0,sd=10)}
 
 
-test <- averagenewdata(pp=pp1,priorf=prior1,pr=pr2,nsamples=100,nshuffles=1000,label='testsimple1',cores=20)
+test <- averagenewdata(pp=pp1,priorf=prior1,pr=pr2,nsamples=100,nshuffles=5000,label='testsimple2',cores=20)
