1f8033d683badf47aad67aa49c7859f2d52d1c1f
luca-hippo <pglpm0@gmail.com>
2018-08-04 11:28:39 +0200

    update data

--- overlearning/j_simple2.R
+++ overlearning/j_simple2.R
@@ -1,12 +1,14 @@
 source('overlearning_simple1.R')
 
-pp0 <- c(c(1/2,1/2)*3/4, 1/4)
+pp0 <- c(c(1/2,1/2)*4/10, 6/10)
 
-prior0 <- function(t){(dnorm(t,mean=6,sd=0.5)+dnorm(t,mean=-6,sd=0.5))/2}
+##prior0 <- function(t){(dnorm(t,mean=6,sd=0.5)+dnorm(t,mean=-6,sd=0.5))/2}
+
+prior0 <- function(t){dnorm(t,mean=0,sd=4)}
 
 ext <- 11
 
 pr0  <- function(t){c(1/(1+ext*exp(t)+exp(2*t)), 1/(1+ext*exp(-t)+exp(-2*t)),
 ext/(ext+exp(-t)+exp(t)))}
 
-test <- averagenewdata(pp=pp0,priorf=prior0,pr=pr0,nsamples=50,nshuffles=1e5,label='testsimple5np4',cores=20,jobname='j_simple2')
+test <- averagenewdata(pp=pp0,priorf=prior0,pr=pr0,nsamples=30,nshuffles=1e5,label='testsimple6cc',cores=20,jobname='j_simple2')
