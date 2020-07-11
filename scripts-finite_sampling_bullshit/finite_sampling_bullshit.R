3de847e8dc959cdf76baa476595e05b09c382af6
luca <piergianluca@gmail.com>
2020-07-11 17:54:02 +0200

    update

--- scripts-finite_sampling_bullshit/finite_sampling_bullshit.R
+++ scripts-finite_sampling_bullshit/finite_sampling_bullshit.R
@@ -47,29 +47,29 @@ freqsampling <- function(freqs,nsamples=20){
 }
 
 
-plotsingle <- function(rfreqs0,filename,tit0,nmcsamples=1000,base=2){
+plotsingle <- function(rfreqs0,filename,tit0,nmcsamples=1000,nbreaks='Sturges',base=2){
     nstim <- ncol(rfreqs0)
     nresp <- nrow(rfreqs0)
     milongrun0 <- mutualinfo(rfreqs0)
 
     misamples0 <- foreach(i=1:nmcsamples,.combine=c)%do%{
         fsample <- foreach(s=1:nstim,.combine=cbind)%do%{
-            tabulate(sample(x=1:nresp,size=20,replace=TRUE,prob=rfreqs0[,s]),nbins=nresp)/20
+            c(tabulate(sample(x=1:nresp,size=20,replace=TRUE,prob=rfreqs0[,s]),nbins=nresp)/20)
         }
         mutualinfo(fsample)
     }
 
-    hires <- hist(misamples0,breaks='Sturges',plot=FALSE)
+    hires <- hist(misamples0,breaks=nbreaks,plot=FALSE)
     maxy <- max(hires$counts)
-    minx <- min(hires$breaks,milongrun0*1.1)
-    maxx <- max(hires$breaks,milongrun0*1.1)
+    minx <- min(hires$breaks,milongrun0)-0.02
+    maxx <- max(hires$breaks,milongrun0)+0.02
     subs <- misamples0[seq(1,length(misamples0),length.out=100)]
     
     pdff(paste0('histo_',filename))
-    matplot(x=subs,y=rep(-maxy/20,100),type='p',lty=1,lwd=3,pch='.',cex=2,col=myblue,xlim=c(minx,maxx),ylim=c(-maxy/20,maxy),xlab=paste0('sampled MI/bit    (sampled mean=',signif(mean(misamples0),2),' bit,   long-run=',signif(milongrun0,2),' bit)'),ylab='',main=tit0)
-    hist(misamples0,breaks='Sturges',xlim=c(minx,maxx),ylim=c(-maxy/20,maxy),xlab='I',ylab='',add=TRUE)
+    matplot(x=subs,y=rep(-maxy/20,100),type='p',lty=1,lwd=3,pch=18,cex=1,col=myblue,xlim=c(minx,maxx),ylim=c(-maxy/20,maxy),xlab=paste0('sampled MI/bit    (sampled mean=',signif(mean(misamples0),3),' bit,   long-run=',signif(milongrun0,3),' bit)'),ylab='',main=tit0)
+    hist(misamples0,breaks=nbreaks,xlim=c(minx,maxx),ylim=c(-maxy/20,maxy),xlab='I',ylab='',add=TRUE)
 ##legend('top',paste0('\nblack: mean, blue: median\nyellow: 16%q'),bty='n')
-    matlines(x=rep(milongrun0,2),y=c(0,maxy),type='l',lty=1,lwd=3,pch='.',col=myred)
+    matlines(x=rep(milongrun0,2),y=c(-maxy/20,maxy),type='l',lty=1,lwd=3,pch='.',col=myred)
     dev.off()
 
     pdff(paste0('resp_',filename))
@@ -78,16 +78,30 @@ plotsingle <- function(rfreqs0,filename,tit0,nmcsamples=1000,base=2){
 }
 
 freqs0 <- matrix(1/10,10,2)
-plotsingle(freqs0,'caseA', 'case A')
+plotsingle(freqs0,'caseA', 'case A',nmcsamples=10000,nbreaks='Sturges')
 
+pt <- 6
+fr <- 100
+f1 <- (c((dcoe(0,n=pt))*fr/100, rep(1/(10-pt),(10-pt))*(100-fr)/100))
+freqs0 <- cbind(rev(f1),f1)
+plotsingle(freqs0,'caseB', 'case B',nmcsamples=10000,nbreaks='Sturges')
+
+pt <- 5
+fr <- 99
+f1 <- c(rev(dcoe(1/pt,n=pt))*fr/100, rep(1/(10-pt),(10-pt))*(100-fr)/100)
+freqs0 <- cbind(rev(f1),(f1))
+plotsingle(freqs0,'caseC', 'case C',nmcsamples=10000,nbreaks='Sturges')
+
+
+
+f1 <- c(rep(1/5,5)*90/100,10/100,rep(0,4))
+freqs0 <- cbind(f1,rev(f1))
+plotsingle(freqs0,'caseB', 'case B',nmcsamples=10000,nbreaks='Sturges')
 
 f1 <- c(rep(1/5,5),rep(0,5))
 freqs0 <- cbind(f1,rev(f1))
 plotsingle(freqs0,'caseB', 'case B')
 
-f1 <- c(rep(1/5,5)*90/100,10/100,rep(0,4))
-freqs0 <- cbind(f1,rev(f1))
-plotsingle(freqs0,'caseB', 'case B')
 
 dcoe <- function(a,n=10){(1-n*a)*2/(n^2+n)*(1:n)+a}
 f1 <- dcoe(0)
@@ -97,20 +111,25 @@ plotsingle(freqs0,'caseC', 'case C')
 
 f1 <- dcoe(0)
 freqs0 <- cbind(f1,rev(f1))
-plotsingle(freqs0,'test', 'case C')
+plotsingle(freqs0,'test', 'case B')
 
-pt <- 6
-fr <- 99
-f1 <- c(rev(dcoe(0,n=pt))*fr/100, rep(1/(10-pt),(10-pt))*(100-fr)/100)
+pt <- 10
+fr <- 100
+f1 <- c(rev(dcoe(1/pt,n=pt))*fr/100, rep(1/(10-pt),(10-pt))*(100-fr)/100)
 freqs0 <- cbind(rev(f1),(f1))
-plotsingle(freqs0,'test', 'case C')
+plotsingle(freqs0,'caseA', 'case A',nmcsamples=10000,nbreaks='Sturges')
 
 
-pt <- 2
+pt <- 7
 fr <- 100
 f1 <- c(rev(dcoe(0,n=pt))*fr/100, rep(1/(10-pt),(10-pt))*(100-fr)/100)
-freqs0 <- cbind(rev(f1),rep(1/10,10))
+freqs0 <- cbind(rev(f1),f1)
 plotsingle(freqs0,'test', 'case C')
+
+
+
+
+
 
 pt <- 8
 fr <- 90
@@ -145,6 +164,53 @@ plotmulti <- function(afreqs,filename,tit0,nmcsamples=1000,base=2){
     pdff(paste0('scatter_',filename))
     par(pty = "s")
     matplot(x=misamples[,1],y=misamples[,2],type='p',lty=1,lwd=3,pch='.',cex=4,col=myblue,xlim=c(0,maxmi),ylim=c(0,maxmi),xlab=paste0('long-run MI/bit'),ylab=paste0('sampled MI/bit'),main=tit0)
+    matlines(x=c(0,maxmi),y=c(0,maxmi),type='l',lty=2,lwd=2,pch='.',col=myyellow)
+    dev.off()
+}
+
+
+plotmulti(rep(10,10),filename='centrepeak',tit0='')
+
+plotmulti(rep(1,10),filename='unif',tit0='')
+
+plotmulti(rep(0.1,10),filename='jeffr',tit0='')
+
+plotmulti(rep(0.1,10),filename='test',tit0='')
+
+
+plotmulti(dcoe(0)*5,filename='test',tit0='')
+
+f1 <- dcoe(0)*1
+plotmulti(cbind(f1,rev(f1)),filename='test',tit0='')
+
+plotmulti(rep(1,10),filename='unif',tit0='')
+
+plotmulti(rep(0.1,10),filename='jeffr',tit0='')
+
+
+
+plotmultimean <- function(afreqs,filename,tit0,nmcsamples=10,nsubsamples=500,base=2){
+    nstim <- 2
+    if(is.null(dim(afreqs))){afreqs <- cbind(afreqs,afreqs)}
+
+    misamples <- foreach(i=1:nmcsamples,.combine=rbind)%do%{
+        rfreqs0 <- cbind(c(rdirichlet(n=1,alpha=afreqs[,1])),
+                         c(rdirichlet(n=1,alpha=afreqs[,2])))
+
+        meanmi <- mean(foreach(i=1:nsubsamples,.combine=c)%do%{
+            fsample <- foreach(s=1:nstim,.combine=cbind)%do%{
+                tabulate(sample(x=1:nresp,size=20,replace=TRUE,prob=rfreqs0[,s]),nbins=nresp)/20
+            }
+            mutualinfo(fsample)
+        })
+        c(mutualinfo(rfreqs0), meanmi)
+    }
+
+    maxmi <- max(c(misamples))
+    pdff(paste0('scatter_',filename))
+    par(pty = "s")
+    matplot(x=misamples[,1],y=misamples[,2],type='p',lty=1,lwd=3,pch='.',cex=4,col=myblue,xlim=c(0,maxmi),ylim=c(0,maxmi),xlab=paste0('long-run MI/bit'),ylab=paste0('sampled MI/bit'),main=tit0)
+    matlines(x=c(0,maxmi),y=c(0,maxmi),type='l',lty=2,lwd=2,pch='.',col=myyellow)
     dev.off()
 }
 
@@ -156,7 +222,7 @@ plotmulti(rep(1,10),filename='unif',tit0='')
 
 plotmulti(rep(0.1,10),filename='jeffr',tit0='')
 
-plotmulti(rep(1,10),filename='test',tit0='')
+plotmultimean(rep(0.1,10),filename='test',tit0='',nmcsamples=30,nsubsamples=500)
 
 
 plotmulti(dcoe(0)*5,filename='test',tit0='')
